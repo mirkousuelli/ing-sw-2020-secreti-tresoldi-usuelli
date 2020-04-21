ba4d272142c062ec1bdcdb21b3518cbc20e29cc7
Mirko Usuelli <mirko.usuelli@mail.polimi.it>
2020-04-21 15:10:03 +0200

    pom.xml moved to top level

---
+++ src/main/java/it/polimi/ingsw/server/model/cards/Deck.java
@@ -0,0 +1,99 @@
+/* *
+ * Project : Santorini
+ * Group : GC15
+ * Author : Riccardo Secreti, Fabio Tresoldi, Mirko Usuelli
+ * Professor : Giampaolo Cugola
+ * Course : Software Engineering Final Project
+ * University : Politecnico di Milano
+ * A.Y. : 2019 - 2020
+ */
+
+package it.polimi.ingsw.server.model.cards;
+
+import it.polimi.ingsw.server.model.cards.xml.GodParser;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.util.*;
+
+public class Deck {
+    /*@class
+     * it contains a list of the 14 chosen cards
+     */
+
+    private final List<Card> cards;
+    private final GodParser parser;
+
+    public Deck() throws ParserConfigurationException, SAXException {
+        /*@constructor
+         * it creates a list of the 14 chosen cards
+         */
+
+        parser = new GodParser(this);
+        cards = new ArrayList<>();
+
+        /*for (Card card : cards) {
+            card = new Card();
+        }*/
+        //test();
+    }
+
+    /*public void test() {
+        List<God> gods = new ArrayList<God>();
+        gods.add(God.APOLLO);
+        gods.add(God.ZEUS);
+
+        fetchCards(gods);
+    }*/
+
+    public Card popRandomCard() {
+        /*@function
+         * it picks a card from the deck
+         */
+
+        Random randomIndex = new Random();
+        Card pickedCard = null;
+
+        if (cards.size() != 0) {
+            pickedCard= cards.get(randomIndex.nextInt(cards.size()));
+            cards.remove(pickedCard);
+        }
+
+        return pickedCard;
+    }
+
+    public void addCard(Card newCard) {
+        this.cards.add(newCard);
+    }
+
+    public void fetchCard(God god) {
+        parser.parseCards(Collections.singletonList(god));
+    }
+
+    public void fetchCards(List<God> gods) {
+        parser.parseCards(gods);
+    }
+
+    public void fetchDeck() {
+        parser.parseCards(Arrays.asList(God.values()));
+    }
+
+    /*public Card popCard(God god) {
+        /*@function
+         * it picks the selected card from the deck
+         *
+
+        Card pickedCard = null;
+
+        if (cards.size() != 0) {
+            for (Card card : cards) {
+                if (card.god.equals(god)){
+                    pickedCard = card;
+                    cards.remove(pickedCard);
+                }
+            }
+        }
+
+        return pickedCard;
+    }*/
+}
---
+++ src/main/java/it/polimi/ingsw/server/model/cards/xml/GodParser.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.server.model.cards.xml;
+
+import it.polimi.ingsw.server.model.cards.Deck;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import it.polimi.ingsw.server.model.cards.God;
+import org.xml.sax.SAXException;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GodParser {
+    private final String XMLFILE = "src/main/java/it/polimi/ingsw/server/model/cards/xml/gods.xml";
+
+    private SAXParserFactory factory;
+    private SAXParser parser;
+    private GodHandler handler;
+
+    public GodParser(Deck deck) throws ParserConfigurationException, SAXException {
+        this.factory = SAXParserFactory.newInstance();
+        this.parser = factory.newSAXParser();
+        this.handler = new GodHandler(deck);
+    }
+
+    public void parseCards(List<God> gods) {
+        try{
+            Collections.sort(gods);
+            this.handler.setGods(gods);
+            this.parser.parse(this.XMLFILE, handler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
